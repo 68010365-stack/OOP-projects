d3ecd3900bae87c9a0e4279e38a66362630229ac
Pange <68010365@kmitl.ac.th>
2026-03-04 10:20:31 +0700

    Initial commit: Add oop_project.py

---
+++ oop_project.py
@@ -0,0 +1,69 @@
+# Actor Class
+class User():
+    pass
+
+class Member(User):
+    pass
+
+class Player(User):
+    pass
+
+
+
+# Main Class
+
+class Server():
+    pass
+
+class Tournament():
+    pass
+
+class Match():
+    pass
+
+class Seat():
+    pass
+
+class OrderSeat():
+    pass
+
+class OrderProduct():
+    pass
+
+
+# Payment Process
+
+class Payment():
+    pass
+
+class Debitcard(Payment):
+    pass
+
+class Cash(Payment):
+    pass
+
+class CurrentAccount():
+    pass
+
+class CounterService():
+    pass
+
+class Coupon():
+    pass
+
+
+#Support class
+class BookedSeat():
+    pass
+
+class Store():
+    pass
+
+class Product():
+    pass
+
+class reciept():
+    pass
+
+class Ticket():
+    pass
